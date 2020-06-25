cdbec8eee6109d1319d3718a6e161d9a735b1141
LauraOchoa <lauraochoae7@gmail.com>
2020-06-24 21:00:25 -0500

    adding responsive design and moment

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
+
+
 //Selectors
 const todoInput = document.querySelector('.todo-input');
 const todoButton = document.querySelector('.todo-button');
 const todoList = document.querySelector('.todo-list');
 const filterOption = document.querySelector('.filter-todo');
+
+const date = document.querySelector('#date');
+date.innerText=moment().format('MMMM Do YYYY');
 
 
 //Listeners
